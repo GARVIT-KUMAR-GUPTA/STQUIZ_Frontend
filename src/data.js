d1fac420bc6ff188af907add377d889826173001
Garvit <garvitguptaggc2013@gmail.com>
2023-03-19 17:48:27 +0530

    Data updated

--- src/data.js
+++ src/data.js
@@ -19,24 +19,24 @@ const questions = [
 			{ answerText: 'Tony Stark', isCorrect: false, Meaning:'Marvel Movie Charactor- Iron Man' },
 		],
 	},
-	// {
-	// 	questionText: 'The iPhone was created by which company?',
-	// 	answerOptions: [
-	// 		{ answerText: 'Apple', isCorrect: true, Meaning:'Apple Created iPhone' },
-	// 		{ answerText: 'Intel', isCorrect: false, Meaning:'Intel Created Computer and Processors' },
-	// 		{ answerText: 'Amazon', isCorrect: false, Meaning:'Amazon is MarketPlace and Cloud service' },
-	// 		{ answerText: 'Microsoft', isCorrect: false, Meaning:'Microsoft Created Operating System' },
-	// 	],
-	// },
-	// {
-	// 	questionText: 'Which Company is reletd to Space Operations?',
-	// 	answerOptions: [
-	// 		{ answerText: 'Isro', isCorrect: true, Meaning:'Isro is a Indian Space Reasearch Organization' },
-	// 		{ answerText: 'Zomato', isCorrect: false, Meaning:'Zomato is Indian Food Delivery Service Company' },
-	// 		{ answerText: 'Paytm', isCorrect: false, Meaning:'Paytm is a digital payment company' },
-	// 		{ answerText: 'Youtube', isCorrect: false, Meaning:'Youtube is a Video Streaming Platform' },
-	// 	],
-	// },
+	{
+		questionText: 'The iPhone was created by which company?',
+		answerOptions: [
+			{ answerText: 'Apple', isCorrect: true, Meaning:'Apple Created iPhone' },
+			{ answerText: 'Intel', isCorrect: false, Meaning:'Intel Created Computer and Processors' },
+			{ answerText: 'Amazon', isCorrect: false, Meaning:'Amazon is MarketPlace and Cloud service' },
+			{ answerText: 'Microsoft', isCorrect: false, Meaning:'Microsoft Created Operating System' },
+		],
+	},
+	{
+		questionText: 'Which Company is reletd to Space Operations?',
+		answerOptions: [
+			{ answerText: 'Isro', isCorrect: true, Meaning:'Isro is a Indian Space Reasearch Organization' },
+			{ answerText: 'Zomato', isCorrect: false, Meaning:'Zomato is Indian Food Delivery Service Company' },
+			{ answerText: 'Paytm', isCorrect: false, Meaning:'Paytm is a digital payment company' },
+			{ answerText: 'Youtube', isCorrect: false, Meaning:'Youtube is a Video Streaming Platform' },
+		],
+	},
 ];
 
 export default questions;
